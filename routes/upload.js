b6f3c0340970a3c0eb79dce8c6b274d68cedc12e
Tush_Raut <168958162+tusharR0@users.noreply.github.com>
2025-07-07 09:49:50 +0530

    Delete routes directory

--- routes/upload.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const multer = require('multer');
-const router = express.Router();
-const upload = multer();
-const { uploadLogo } = require('../controllers/uploadController');
-
-router.post('/logo', upload.single('file'), uploadLogo);
-
-module.exports = router;
